86bb9a572d09f55596f7fc50f5b4068c428bbdc1
tankTopTaro <109114490+tankTopTaro@users.noreply.github.com>
2022-10-05 03:12:55 +0800

    Update my_helper_functions.py

--- helper_functions/my_helper_functions.py
+++ helper_functions/my_helper_functions.py
@@ -1,1 +1,15 @@
+import tensorflow as tf
 
+def create_checkpoint_callback(checkpoint_path):
+  """
+  Creates a ModelCheckpoint callback
+  Args:
+    checkpoint_path: target directory or name to store ModelCheckpoint files
+  """
+  checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+    checkpoint_path,
+    save_weights_only = True,
+    monitor = 'val_accuracy',
+    save_best_only = True
+  )
+  return checkpoint_callback
